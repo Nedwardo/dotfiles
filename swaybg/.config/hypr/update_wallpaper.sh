ff1cd995a14f9c0c03d7854acf52aaf2d8b4342a
Nedwardo <edwardbo123@gmail.com>
2025-10-11 13:58:11 +0100

    Updated swaybg to run from .local/bin and added some options
    Signed-off-by: Nedwardo <edwardbo123@gmail.com>

--- swaybg/.config/hypr/update_wallpaper.sh
+++ swaybg/.config/hypr/update_wallpaper.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 shopt -s nullglob
-wallpaper_dir=$HOME/documents/wallpapers
+CONFIG_FILE="$HOME/.config/swaybg.json"
+declare -gA CONFIG
 
-images=($wallpaper_dir/*.{png,jpg,jpeg,xl,webp})
+function config(){
+	echo "$CONFIG_FILE"
+}
 
-random=$$$(date +%s)
-export wallpaper=${images[$random % ${#images[@]}]}
+function load_config(){
+	while IFS="=" read -r key value; do
+		CONFIG[$key]="$value"
+	done < <(jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' "$CONFIG_FILE")
+}
 
-if pgrep swaybg &> /dev/null; then
-	pkill swaybg &> /dev/null
-fi
-swaybg -i $wallpaper -m fit &> /dev/null &
+function get_active_wallpaper(){
+	readlink "$CONFIG[wallpaper_dir]"/active_wallpaper
+}
 
-ln -sf $wallpaper $wallpaper_dir/active_wallpaper
+function select_random_wallpaper(){
+	if [ "$#" = 0 ]; then
+		wallpaper_dir="$CONFIG[wallpaper_dir]"
+	else
+		wallpaper_dir=$1
+	fi
 
+	images=($wallpaper_dir/*.{png,jpg,jpeg,xl,webp})
+
+	random=$$$(date +%s)
+	echo ${images[$random % ${#images[@]}]}
+}
+
+function run_singleton_sway() {
+	if [ "$#" != 1 ]; then
+		>&2 echo "Please only select at least one, and only one wallpaper"
+	fi
+	wallpaper=$1
+
+	if pgrep swaybg &> /dev/null; then
+		pkill swaybg &> /dev/null
+	fi
+	swaybg -i $wallpaper -m $CONFIG[default_mode] &> /dev/null &
+
+	rm $wallpaper_dir/active_wallpaper
+	ln -sf $wallpaper $wallpaper_dir/active_wallpaper
+}
+
+load_config
+run_singleton_sway $(select_random_wallpaper)
 echo $wallpaper

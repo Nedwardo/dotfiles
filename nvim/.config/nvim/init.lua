6a62a5dd8cfd1e4cdbd4b825f9ac10cae8def14a
Nedwardo <edwardbo123@gmail.com>
2025-11-15 12:31:57 +0000

    Configured a little bit of nivm

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,12 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 vim.o.number = true
 vim.o.relativenumber = true
 
 vim.schedule(function()
 	vim.o.clipboard ='unnamedplus'
 end)
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -15,7 +15,7 @@ vim.g.inverse_scrolloff_size = 5
 local function set_dynamic_scrolloff()
 	vim.opt.scrolloff = math.ceil(vim.o.lines / 2) - (vim.g.inverse_scrolloff_size + 1)
 	-- On odd line count: scroll_off_size*2 - 1 lines
-	-- On even: scroll_off_size*2 lines
+	-- On even: scroll_off_size*2 line
 end
 
 vim.api.nvim_create_autocmd("VimResized", {
@@ -26,3 +26,7 @@ vim.api.nvim_create_autocmd("VimResized", {
 set_dynamic_scrolloff()
 
 vim.o.undofile = true
+
+require("lazy-bootstrap")
+require("lazy").setup("plugins")
+require("keybinds")

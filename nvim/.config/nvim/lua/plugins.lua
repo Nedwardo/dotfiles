67787929afc25c74254764fc4085c74c67a7c54f
Nedwardo <edwardbo123@gmail.com>
2026-01-16 14:11:12 +0000

    nvim lsp stuff

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,11 +1,35 @@
 return {
-  	"neovim/nvim-lspconfig",
-  	{
-	  	"nvim-telescope/telescope.nvim",
-		dependencies = { 'nvim-lua/plenary.nvim' }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
+		build = ":tsupdate",
 	},
 	{
-		"mason-org/mason.nvim",
-		opts = {}
-	}
+		"neovim/nvim-lspconfig",
+		dependencies = { "mason-org/mason-lspconfig.nvim" },
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	"stevearc/conform.nvim",
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+	},
 }

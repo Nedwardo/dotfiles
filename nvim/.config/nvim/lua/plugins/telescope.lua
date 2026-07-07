de6edf0824d2384647a77a726a990157397ae0fe
Nedwardo <edwardbo123@gmail.com>
2026-07-07 16:44:03 +0100

    Updated nvim stuff

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,15 +4,24 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		},
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
 		},
 		config = function()
-			require("telescope").load_extension("ui-select")
+			local telescope = require("telescope")
+			telescope.setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_cursor({
+							layout_config = {
+								width = 60,
+								height = 12,
+							},
+						}),
+					},
+				},
+			})
+			telescope.load_extension("fzf")
+			telescope.load_extension("ui-select")
 		end,
 	},
 }
